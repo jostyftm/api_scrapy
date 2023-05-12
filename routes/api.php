03eaa863abfe0464b6359254c246d2bca0272350
Joe Valencia <stiwart@gmail.com>
2023-05-12 11:42:05 -0500

    change namespace Search Resource

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\ResourceController;
 use App\Http\Controllers\Api\ResourceTypeController;
 use App\Http\Controllers\Api\SearchController;
-use App\Http\Controllers\api\SearchResourceController;
+use App\Http\Controllers\Api\SearchResourceController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\WebsiteController;
 use Illuminate\Http\Request;
